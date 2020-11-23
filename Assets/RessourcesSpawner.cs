1bf257eb9e6632248ef407ddcacb5db0ccdf1f50
Timcampy <17752538+Timcampy@users.noreply.github.com>
2020-11-23 18:08:47 +0100

    temp

---
+++ Assets/RessourcesSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class RessourcesSpawner : MonoBehaviour
+{
+    public Tilemap m_tilemap;
+    public ScriptableObject m_mapScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
