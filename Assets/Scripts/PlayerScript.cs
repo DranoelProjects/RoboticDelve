cec2d9a2727c8b583b445fdfa85fda343fab224b
Timcampy <17752538+Timcampy@users.noreply.github.com>
2020-12-07 00:53:01 +0100

    bruteforce suite

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,8 +10,10 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] public float healthpoints = 8;
     [SerializeField] public float healthpointsMax = 10;
 
-    public Transform m_spriteTransform;
+    GameManagerScript gameManagerScript;
+    public bool CanMoove = true;
 
+    public Transform m_spriteTransform;
     bool lookRight = true;
     public bool OnAttack = false;
 
@@ -37,47 +39,64 @@ public class PlayerScript : MonoBehaviour
         audioSource = GetComponentInChildren<AudioSource>();
         inventoryManager = GameObject.Find("InventoryManager").GetComponent<InventoryManager>();
         uiScript = GameObject.Find("MainCanvas").GetComponent<UIScript>();
+        gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        //movements
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        float actualSpeed = speed;
-        if (moveX != 0 && moveY != 0)
-            actualSpeed = speed / Mathf.Sqrt(2);
-        //m_rigidBody2D.MovePosition(new Vector2(m_rigidBody2D.position.x + moveX * actualSpeed * Time.deltaTime, m_rigidBody2D.position.y + moveY * actualSpeed * Time.deltaTime));
-        transform.Translate(Vector2.right * moveX * actualSpeed * Time.deltaTime);
-        transform.Translate(Vector2.up * moveY * actualSpeed * Time.deltaTime);
-        animator.SetFloat("SpeedX", Mathf.Abs(moveX));
-        animator.SetFloat("SpeedY", moveY);
-
-        if (moveX > 0 && !lookRight)
-            Flip();
-        else if (moveX < 0 && lookRight)
-            Flip();
-
-        if (Input.GetKeyDown(KeyCode.Mouse0) && !OnAttack && isAbleToAttack)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            OnAttack = true;
-            animator.SetTrigger("Attack");
-            audioSource.PlayOneShot(sndAttack);
+            uiScript.PauseGame();
         }
-
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             uiScript.PanelInventory.SetActive(!uiScript.PanelInventory.activeInHierarchy);
             uiScript.UpdateInventoryUI();
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            uiScript.PauseGame();
+            gameManagerScript.SwapBetweenRobotAndPlayer();
         }
-
-        DeathPlayer();
     }
+
+    void FixedUpdate()
+    {
+        if (CanMoove)
+        {
+            //movements
+            float moveX = Input.GetAxis("Horizontal");
+            float moveY = Input.GetAxis("Vertical");
+            float actualSpeed = speed;
+            if (moveX != 0 && moveY != 0)
+                actualSpeed = speed / Mathf.Sqrt(2);
+            //m_rigidBody2D.MovePosition(new Vector2(m_rigidBody2D.position.x + moveX * actualSpeed * Time.deltaTime, m_rigidBody2D.position.y + moveY * actualSpeed * Time.deltaTime));
+            transform.Translate(Vector2.right * moveX * actualSpeed * Time.deltaTime);
+            transform.Translate(Vector2.up * moveY * actualSpeed * Time.deltaTime);
+            animator.SetFloat("SpeedX", Mathf.Abs(moveX));
+            animator.SetFloat("SpeedY", moveY);
+
+            if (moveX > 0 && !lookRight)
+            {
+                Flip();
+            }
+            else if (moveX < 0 && lookRight)
+            {
+                Flip();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0) && !OnAttack && isAbleToAttack)
+            {
+                OnAttack = true;
+                animator.SetTrigger("Attack");
+                audioSource.PlayOneShot(sndAttack);
+                StartCoroutine(AttackBool());
+            }
+
+            DeathPlayer();
+        }
+    }
+
 
     void Flip()
     {
@@ -87,10 +106,12 @@ public class PlayerScript : MonoBehaviour
         m_spriteTransform.localScale = theScale;
     }
 
-    public void AttackBool()
+    IEnumerator AttackBool()
     {
+        yield return new WaitForSeconds(0.3f);
         OnAttack = false;
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -107,6 +128,12 @@ public class PlayerScript : MonoBehaviour
             {
                 uiScript.UpdateInventoryUI();
             }
+            if (itemName == "RobotPlan")
+            {
+                RobotPlanScript robotPlanScript = collision.gameObject.GetComponent<RobotPlanScript>();
+                inventoryManager.AddNewPlan(robotPlanScript.CurrentRobot);
+            }
+
         }
     }
 
