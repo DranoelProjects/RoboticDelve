4e9aea135449558f0e0a11a81b7bf3f06c22d3e4
CrumblingGolem <17752538+Timcampy@users.noreply.github.com>
2020-12-20 22:09:40 +0100

    generation fonctionnelle

---
+++ Assets/Scripts/Generator/Intermediaire.cs
@@ -0,0 +1,170 @@
+﻿//using QM2D.Generator;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Threading;
+//using UnityEngine;
+
+//using Color = System.Drawing.Color;
+//using WFCInput = QM2D.Generator.Input;
+
+//public class Intermediaire : MonoBehaviour
+//{
+//    private Color[,] _data, colorOutput;
+//    private Thread myThread;
+//    private bool goToNextScene, stopThread, toutFini, showProgBar;
+//    public int outputHeight, outputWidth, maxEntropy, currentEntropy;
+//    private WFCInput myInput;
+//    private State myState;
+//    private int[,] finalOutput;
+//    void Start()
+//    {
+//        Application.targetFrameRate = 60;
+//        goToNextScene = false;
+//        stopThread = false;
+//        toutFini = false;
+//        showProgBar = false;
+//        myThread = new Thread(startProcess);
+//        myThread.Start();
+//    }
+    
+//    void FixedUpdate()
+//    {
+//        if (toutFini)
+//        {
+//            Debug.Log(displayText(finalOutput));
+//            toutFini = false;
+//        }
+//        //if (showProgBar)
+//        //{
+//        //}
+//    }
+
+//    private void OnApplicationQuit()
+//    {
+//        stopThread = true;
+//    }
+
+//    public void startProcess()
+//    {
+//        int[,] input = new int[,]{  {0, 0, 0, 0, 0, 0, 0, 0},
+//                                    {0, 1, 1, 1, 1, 0, 0, 0},
+//                                    {0, 1, 0, 0, 1, 0, 0, 0},
+//                                    {0, 1, 0, 0, 1, 0, 0, 0},
+//                                    {0, 1, 1, 0, 1, 0, 1, 1},
+//                                    {0, 0, 0, 0, 1, 0, 0, 1},
+//                                    {0, 0, 0, 0, 1, 0, 0, 1},
+//                                    {0, 0, 0, 0, 1, 1, 1, 1} };
+                                    
+//        int height = input.GetLength(0), width = input.GetLength(1);
+//        _data = intToColor(input, height, width);
+//        Vector2i outputDim = new Vector2i(outputHeight, outputWidth);
+//        myInput = new WFCInput(_data, new Vector2i(3, 3), false, false, false, false);
+//        System.Random rng = new System.Random();
+//        int seed = rng.Next();
+//        myState = new State(myInput, outputDim, false, false, seed);
+//        HashSet<Vector2i> failPoints = new HashSet<Vector2i>();
+//        bool isGen = false;
+//        int echecs = 0;
+//        maxEntropy = myState.getTotEntropy();
+//        showProgBar = true;
+//        while (!isGen)
+//        {
+//            var etat = myState.Iterate(ref failPoints);
+//            if (etat == true)
+//            {
+//                isGen = true;
+//            }
+//            if (etat == false)
+//            {
+//                seed = rng.Next();
+//                myState.Reset(null, seed);
+//                echecs++;
+//                if (echecs > 16)
+//                {
+//                    stopThread = true;
+//                }
+//            }
+//            currentEntropy = myState.getTotEntropy();
+//            if (stopThread)
+//            {
+//                return;
+//            }
+//        }
+//        colorOutput = getColorOutput(height, width);
+//        finalOutput = colorToInt(colorOutput, height, width);
+//        toutFini = true;
+//    }
+
+//    public Color[,] intToColor(int[,] intData, int height, int width)
+//    {
+//        Color[,] ret = new Color[height, width];
+//        int pixelColor = 0;
+//        for (int i = 0; i < height; i++)
+//        {
+//            for (int j = 0; j < width; j++)
+//            {
+//                if (intData[i, j] == 0)
+//                {
+//                    pixelColor = 0;
+//                }
+//                else if (intData[i, j] == 1)
+//                {
+//                    pixelColor = 255;
+//                }
+//                ret[i, j] = Color.FromArgb(pixelColor, pixelColor, pixelColor);
+//            }
+//        }
+//        return ret;
+//    }
+
+//    public int[,] colorToInt(Color[,] colorData, int height, int width)
+//    {
+//        int[,] ret = new int[height, width];
+//        for (int i = 0; i < height; i++)
+//        {
+//            for (int j = 0; j < width; j++)
+//            {
+//                if (colorData[i, j].B == 0)
+//                {
+//                    ret[i, j] = 0;
+//                }
+//                else if (colorData[i, j].B == 255)
+//                {
+//                    ret[i, j] = 1;
+//                }
+//            }
+//        }
+//        return ret;
+//    }
+//    public Color[,] getColorOutput(int height, int width)
+//    {
+//        Color[,] ret = new Color[height, width];
+//        for (int i = 0; i < height; i++)
+//        {
+//            for (int j = 0; j < width; j++)
+//            {
+//                var col = myState.OutputColorGetter(new Vector2i(i, j));
+//                if (col == null)
+//                {
+//                    col = Color.FromArgb(0, 0, 0);
+//                }
+//                ret[i, j] = (Color) col;
+//            }
+//        }
+//        return ret;
+//    }
+
+//    string displayText(int[,] mat)
+//    {
+//        string sortie = "";
+//        for (int j = 0; j < mat.GetLength(0); j++)
+//        {
+//            for (int i = 0; i < mat.GetLength(1); i++)
+//            {
+//                sortie += mat[j, i] + ",";
+//            }
+//            sortie += "\n";
+//        }
+//        return sortie;
+//    }
+//}
--- Assets/Scripts/WFCOpti/Stuff.cs
+++ Assets/Scripts/WFCOpti/Stuff.cs
@@ -6,57 +6,43 @@ The above copyright notice and this permission notice shall be included in all c
 The software is provided "as is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.
 */
 
-using System;
-using System.Xml;
 using System.Linq;
+using System.Xml.Linq;
 using System.ComponentModel;
 using System.Collections.Generic;
 
 static class Stuff
 {
-	public static int Random(this double[] a, double r)
-	{
-		double sum = a.Sum();
+    public static int Random(this double[] a, double r)
+    {
+        double sum = a.Sum();
+        for (int j = 0; j < a.Length; j++) a[j] /= sum;
 
-		if (sum == 0)
-		{
-			for (int j = 0; j < a.Count(); j++) a[j] = 1;
-			sum = a.Sum();
-		}
+        int i = 0;
+        double x = 0;
 
-		for (int j = 0; j < a.Count(); j++) a[j] /= sum;
+        while (i < a.Length)
+        {
+            x += a[i];
+            if (r <= x) return i;
+            i++;
+        }
 
-		int i = 0;
-		double x = 0;
+        return 0;
+    }
 
-		while (i < a.Count())
-		{
-			x += a[i];
-			if (r <= x) return i;
-			i++;
-		}
+    public static long ToPower(this int a, int n)
+    {
+        long product = 1;
+        for (int i = 0; i < n; i++) product *= a;
+        return product;
+    }
 
-		return 0;
-	}
+    public static T Get<T>(this XElement xelem, string attribute, T defaultT = default)
+    {
+        XAttribute a = xelem.Attribute(attribute);
+        return a == null ? defaultT : (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromInvariantString(a.Value);
+    }
 
-	public static long Power(int a, int n)
-	{
-		long product = 1;
-		for (int i = 0; i < n; i++) product *= a;
-		return product;
-	}
-
-	public static T Get<T>(this XmlNode node, string attribute, T defaultT = default(T))
-	{
-		string s = ((XmlElement)node).GetAttribute(attribute);
-		var converter = TypeDescriptor.GetConverter(typeof(T));
-		return s == "" ? defaultT : (T)converter.ConvertFromString(s);
-	}
-
-	public static T[] SubArray<T>(this T[] data, int index, int length)
-	{
-	    T[] result = new T[length];
-	    Array.Copy(data, index, result, 0, length);
-	    return result;
-	}
+    public static IEnumerable<XElement> Elements(this XElement xelement, params string[] names) => xelement.Elements().Where(e => names.Any(n => n == e.Name));
 }
